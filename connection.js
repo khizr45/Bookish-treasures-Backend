2b79593b127bf59c782b7029260ed3498a7115ca
Muhammad Khizar <79244644+khizr45@users.noreply.github.com>
2025-10-05 21:08:26 +0500

    Final app

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
-var mysql = require("mysql");
+var mysql = require("mysql2");
 var con = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"bookish-treasures"
+    host:"bl45uisedcenkiwkqlta-mysql.services.clever-cloud.com",
+    user:"uhelc1ly83ajbrnk",
+    password:"QLn3ZxB16PhJhz4rsRc8",
+    database:"bl45uisedcenkiwkqlta"
 })
 
 con.connect((error)=>{
